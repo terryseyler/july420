4348eb43bb05a6a12394abfdde0b079906a66328
Terry <tlseyler@gmail.com>
2022-06-17 16:27:45 +0000

    adjusted api to pull id's

--- scripts/song_pull.py
+++ scripts/song_pull.py
@@ -7,7 +7,6 @@
 import requests
 import json
 from datetime import datetime
-from sqlite3 import Error
 import sqlite3
 import time
 
@@ -25,20 +24,20 @@ def create_connection():
         conn = sqlite3.connect(file
         ,detect_types=sqlite3.PARSE_DECLTYPES)
         #conn.row_factory=sqlite3.Row
-      
+
         return conn
 
-    except Error as e:
-        print("could not open pythonanywhere db".format(e))
+    except:
+        print("could not open pythonanywhere db")
         try:
             file="/Users/terryseyler/Library/CloudStorage/OneDrive-Personal/git/july420/DB/july420.db"
             conn = sqlite3.connect(file
             ,detect_types=sqlite3.PARSE_DECLTYPES)
             #conn.row_factory=sqlite3.Row
-           
+
             return conn
-        except Error as e:
-            print("could not open local db".format(e))
+        except:
+            print("could not open local db")
 
 
 # In[38]:
@@ -57,6 +56,9 @@ def pull_songs():
             j = json.loads(r.text)
 
             #parse json
+            albumid = str(j['albumId'])
+            artistid = str(j['artistId'])
+            trackid = str(j['trackId'])
             song = j['title']
             band=j['artist']
             album = j['album']
@@ -70,9 +72,9 @@ def pull_songs():
         conn=create_connection()
         try:
             conn.execute(
-                  'INSERT OR IGNORE INTO july2022 (song,band,album,datetime,like)'
-                  ' VALUES (?, ?, ?, ?,?)',
-                   (song,band,album,date_time,0)
+                  'INSERT OR IGNORE INTO july2022 (song,band,album,datetime,like,albumid,artistid,trackid)'
+                  ' VALUES (?, ?, ?, ?,?,?,?,?)',
+                   (song,band,album,date_time,0,albumid,artistid,trackid)
                         )
             conn.execute("update july2022 set last_updated ='{}'".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")))
             conn.commit()
